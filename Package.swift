0fb536fd207f7a2aea5ef628d7c98fa5484a2a5d
CI <ci@banuba.com>
2025-01-23 14:16:50 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,16 +2,21 @@
 
 import PackageDescription
 
+let utilsVersion: Version = "1.39.0"
+
 let package = Package(
   name: "BanubaLicenseServicingSDK",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaLicenseServicingSDK",
       targets: ["BanubaLicenseServicingSDK"]
     )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BNBLicenseUtils-iOS.git", from: utilsVersion),
   ],
   targets: [
     .binaryTarget(
